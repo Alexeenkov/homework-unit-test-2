e792cbc202334c30351ae3dd4af526544386aa96
Ilya Alekseenkov <rmfighht@yandex.ru>
2021-08-06 10:04:32 +0300

    Выполнил 2 задание в соответствии с указаниями преподавателя

--- src/js/__tests__/test-app.js
+++ src/js/__tests__/test-app.js
@@ -1,6 +1,27 @@
 import sortByHealth from '../app';
 
-test('charaters sort by health', () => {
+test('charaters sort by health (with .toBe())', () => {
+  const input = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  const received = sortByHealth(input);
+
+  expect(received).toBe(expected);
+  /* .toBe() используется для сравнения примитивных значений или проверки
+  референциальной идентичности экземпляров объекта. Он вызывает Object.is
+  для сравнения значений. Не работает для сравнения объектов. */
+});
+
+test('charaters sort by health (with .toEqual())', () => {
   const input = [
     { name: 'мечник', health: 10 },
     { name: 'маг', health: 100 },
@@ -16,4 +37,7 @@ test('charaters sort by health', () => {
   const received = sortByHealth(input);
 
   expect(received).toEqual(expected);
+  /* .toEqual используется для рекурсивного сравнения всех свойств экземпляров объектов
+  (также известное как "глубокое" равенство). Также, как и .toBe() вызывает Object.is
+  для сравнения примитивных значений */
 });
